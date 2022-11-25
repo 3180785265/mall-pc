442ee44e8309a5b52df091ec550f4b0ae2eb9edd
liujj <3180785265@qq.com>
2022-11-25 15:04:37 +0700

    会员

--- mall-portal/src/main/java/com/itheima/mall/controller/OmsCartItemController.java
+++ mall-portal/src/main/java/com/itheima/mall/controller/OmsCartItemController.java
@@ -1,6 +1,13 @@
 package com.itheima.mall.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.itheima.mall.common.R;
+import com.itheima.mall.domain.OmsCartItem;
+import com.itheima.mall.domain.PmsSkuStock;
+import com.itheima.mall.service.IPmsSkuStockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +23,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cartItem")
 public class OmsCartItemController {
 
+    @Autowired
+    private IPmsSkuStockService iPmsSkuStockService;
+
+    /**
+     * 添加商品到购物车
+     *
+     * @return
+     */
+    @RequestMapping("/add")
+    public R add(@RequestBody OmsCartItem cartItem) {
+
+        //获取商品sku
+        LambdaQueryWrapper<PmsSkuStock> pmsSkuStockLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        pmsSkuStockLambdaQueryWrapper.eq(PmsSkuStock::getProductId, cartItem.getProductId());
+        pmsSkuStockLambdaQueryWrapper.eq(PmsSkuStock::getSkuCode, cartItem.getProductSkuCode());
+        PmsSkuStock skuStock = iPmsSkuStockService.getOne(pmsSkuStockLambdaQueryWrapper);
+
+        //获取登录会员
+        ums_member
+
+    }
 }
 
